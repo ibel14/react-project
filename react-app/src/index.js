5da3d8f93a5a4dcca12ff554826c2d0733a49f21
ponaehali <iambelousov@gmail.com>
2021-01-07 15:08:00 +0200

    #89 almost

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,44 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-
-const Header = () => {
-  return <h2>Hello ninja!</h2>
-}
-
-const Field = () => {
-  const holder = "Enter here";
-  const styledField = {
-    width: '300px'
-  }
-
-  return <input 
-              style={styledField}
-              type="text" 
-              placeholder={holder} 
-              autoComplete=""
-              className="first"
-              htmlFor="" />
-}
-
-const Btn = () => {
-  const text = "Log in";
-  const logged = true;
-  // const res = () => {
-  //   return 'Log in, please'
-  // }
-return <button>{logged ? "Enter" : text}</button>
-}
-
-const App = () => {
-  return (
-    <div>
-    <Header/>
-    <Field/>
-    <Btn/>
-    </div>
-  )
-}
+import App from './components/app/';
 
 ReactDOM.render(<App/>, document.getElementById('root'));

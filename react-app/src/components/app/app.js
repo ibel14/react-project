7401378e4a96f0fcd071e8c6277711b00ffad30a
ponaehali <iambelousov@gmail.com>
2021-01-10 02:28:38 +0200

    #95 created status filter, completed the basic level

--- react-app/src/components/app/app.js
+++ react-app/src/components/app/app.js
@@ -18,13 +18,15 @@ export default class App extends Component {
                 {label: "How about pizza?", important: false, like: false, id: 2},
                 {label: "My first twitt!", important: false, like: false, id: 3}
             ],
-            term: ''
+            term: '',
+            filter: 'all'
         };
         this.deteleItem = this.deteleItem.bind(this);
         this.addItem = this.addItem.bind(this);
         this.onToggleImportant = this.onToggleImportant.bind(this);
         this.onToggleLiked = this.onToggleLiked.bind(this);
         this.onUpdateSearch = this.onUpdateSearch.bind(this);
+        this.onFilterSelect = this.onFilterSelect.bind(this);
 
         this.maxId = 4;
     }
@@ -95,17 +97,29 @@ export default class App extends Component {
         });
     }
 
+    filterPost(items, filter) {
+        if (filter === 'like') {
+            return items.filter(item => item.like)
+        } else {
+            return items
+        }
+    }
+
     onUpdateSearch(term) {
         this.setState({term})
     }
 
+    onFilterSelect(filter) {
+        this.setState({filter})
+    }
+
     render() {
-        const {data, term} = this.state;
+        const {data, term, filter} = this.state;
 
         const liked = data.filter(item => item.like).length;
         const allPosts = data.length;
 
-        const visiblePosts = this.searchPost(data, term);
+        const visiblePosts = this.filterPost(this.searchPost(data, term), filter);
 
         return (
             <div className="app">
@@ -115,7 +129,9 @@ export default class App extends Component {
                     <div className="search-panel d-flex">
                     <SearchPanel
                     OnUpdateSearch={this.OnUpdateSearch}/>
-                    <PostStatusFilter/>
+                    <PostStatusFilter
+                    filter={filter}
+                    onFilterSelect={this.onFilterSelect}/>
                     </div>
                     <PostList
                      posts={visiblePosts}
